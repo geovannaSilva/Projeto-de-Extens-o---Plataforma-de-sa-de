1d38c52fbcb4c07b10a91102aefd483b21e311d3
geovannaSilva <54729023+geovannaSilva@users.noreply.github.com>
2025-09-08 15:07:05 -0300

    Novas alterações
    Alterações realizadas até o dia 08/09/2025

--- cadastro.py
+++ cadastro.py
@@ -5,6 +5,7 @@ idades = []
 pacientes = []
 
 while True:
+    print("\nInforme a opção desejada:")
     print('\n1.Cadastrar paciente\n'
           '2.Ver estatística\n'
           '3.Buscar paciente\n'
@@ -21,7 +22,8 @@ while True:
         idades.append((idade))
         telefone = str(input('Telefone: '))
 
-        pacientes.append({"nome": nomepaciente, "idade": idade})
+
+        pacientes.append({"nome": nomepaciente, "idade": idade, "telefone":telefone})
 
         print('\nPaciente cadastrado com sucesso!')
 
@@ -41,6 +43,24 @@ while True:
         print(f'O paciente mais novo é {pac_novo['nome']} e tem {pac_novo['idade']} anos.')
         print(f'O paciente mais velho é {pac_velho['nome']} e tem {pac_velho['idade']} anos')
 
+    if escolha == 3:
+        if pacientes:
+            busca_paciente = str(input("\nDigite o nome do paciente que deseja buscar: "))
+            encontrado = False
+
+            for p in pacientes:
+                if p["nome"].lower() == busca_paciente.lower():
+                    print(f"Paciente encontrado: \n"f"{p['nome']} \n{p['idade']} anos \n{p['telefone']}")
+                    encontrado = True
+
+            if not encontrado:
+                print(f"O Paciente {busca_paciente} não foi cadastrado!")
+
+
+    if escolha == 4:
+        print(f"\nOs pacientes cadastrados são: \n{pacientes}")
+
+
     if escolha == 5:
-        print('\nSainda do programa...')
+        print('\nPrograma encerrado!')
         break
